72a3a5d54219fc50e7c5010a2c2d3f9f08c33661
LQ <liqiangeastsun@163.com>
2022-08-24 20:52:53 +0800

    no message

--- UIFrame/Assets/Scripts/UI/LoopScrollView/LoopScrollView.cs
+++ UIFrame/Assets/Scripts/UI/LoopScrollView/LoopScrollView.cs
@@ -68,76 +68,9 @@ public class LoopScrollView : MonoBehaviour
         _scrollRect.normalizedPosition = position;
     }
 
-    /*
-    private Vector2 lastPoint = Vector2.zero;
-    private void ScrollChange(Vector2 pos)
+    public void GoToIndex(int index)
     {
-        for (int i = 0; i < _itemList.Count; ++i)
-        {
-            //Transform child = _itemList[i];
-            //Vector2 screenPoint = PositionConvert.UIPointToScreenPoint(child.position);
-            //Vector2 localPos = PositionConvert.ScreenPointToUILocalPoint(_rectTransform, screenPoint);
-            //if (pos.y < lastPoint.y)  // 向上
-            //{
-            //    Up(child, localPos);
-            //}
-            //else   // 向下
-            //{
-            //    Down(child, localPos);
-            //}
-        }
 
-        lastPoint = pos;
     }
-
-    private void Up(Transform child, Vector2 localPos)
-    {
-        float y = _rect.sizeDelta.y * 0.5f + _itemSize.y * 0.5f;
-        if (localPos.y <= y)
-        {
-            return;
-        }
-        Debug.LogError("向上:" + child.name + "    " + localPos.y + "    y:" + y);
-        child.SetAsLastSibling();
-        int index = int.Parse(child.name);
-        index += 5;
-        ResetPoint(child, index);
-    }
-
-    private void Down(Transform child, Vector2 localPos)
-    {
-        float y = _rect.sizeDelta.y * -0.5f - _itemSize.y;
-        if (localPos.y >= y)
-        {
-            return;
-        }
-
-        Debug.LogError("向下");
-        int index = int.Parse(child.name);
-        index -= 5;
-        if (index < 0)
-        {
-            return;
-        }
-        child.SetAsFirstSibling();
-        ResetPoint(child, index);
-    }
-
-    private void ResetPoint(Transform child, int index)
-    {
-        RectTransform rt = child.GetComponent<RectTransform>();
-        Debug.LogError("1:" + child.name + "   " + rt.anchoredPosition);
-
-        child.name = index.ToString();
-        Vector2 point = new Vector2(50, -50 - index * 110);
-        rt.anchoredPosition = point;
-
-        Debug.LogError("2:" + child.name + "    " + rt.anchoredPosition);
-        //Vector2 screenPoint = PositionConvert.UIPointToScreenPoint(child.position);
-        //Vector2 localPos = PositionConvert.ScreenPointToUILocalPoint(_rectTransform, screenPoint);
-
-        //Debug.LogError("3:" + child.name + "     " + localPos);
-    }
-    */
 
 }
